272eaf0709ee6f72aa8479836abdc0ed768d1a92
Coda <cody.koelliker@outlook.com>
2021-12-10 17:17:26 -0700

    more 1.18 port

--- src/main/java/teamdraco/dusted/client/render/DustBunnyRenderer.java
+++ src/main/java/teamdraco/dusted/client/render/DustBunnyRenderer.java
@@ -1,35 +1,37 @@
 package teamdraco.dusted.client.render;
 
-import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.MobRenderer;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import teamdraco.dusted.BellyButton;
 import teamdraco.dusted.client.model.DustBunnyModel;
 import teamdraco.dusted.common.entities.DustBunnyEntity;
-import net.minecraft.client.renderer.entity.EntityRendererManager;
-import net.minecraft.client.renderer.entity.MobRenderer;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.text.TextFormatting;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
 public class DustBunnyRenderer extends MobRenderer<DustBunnyEntity, DustBunnyModel<DustBunnyEntity>> {
     private static final ResourceLocation TEXTURE = new ResourceLocation(BellyButton.MOD_ID, "textures/entity/dust_bunny.png");
     private static final ResourceLocation BIG_CHUNGUS = new ResourceLocation(BellyButton.MOD_ID, "textures/entity/big_chungus.png");
+    public static final ModelLayerLocation DUST_BUNNY = new ModelLayerLocation(new ResourceLocation(BellyButton.MOD_ID, "dust_bunny"), "main");
 
-    public DustBunnyRenderer(EntityRendererManager renderManagerIn) {
-        super(renderManagerIn, new DustBunnyModel<>(), 0.2F);
+    public DustBunnyRenderer(EntityRendererProvider.Context manager) {
+        super(manager, new DustBunnyModel<>(manager.bakeLayer(DUST_BUNNY)), 0.2F);
     }
 
-    protected void scale(DustBunnyEntity entity, MatrixStack matrixStackIn, float partialTickTime) {
+    protected void scale(DustBunnyEntity entity, PoseStack matrixStackIn, float partialTickTime) {
         float f1 = (float)entity.getSize();
-        float f2 = MathHelper.lerp(partialTickTime, entity.prevSquishFactor, entity.squishFactor) / (f1 * 0.5F + 1.0F);
+        float f2 = Mth.lerp(partialTickTime, entity.prevSquishFactor, entity.squishFactor) / (f1 * 0.5F + 1.0F);
         float f3 = 1.0F / (f2 + 1.0F);
         matrixStackIn.scale(f3 * f1, 1.0F / f3 * f1, f3 * f1);
     }
 
     public ResourceLocation getTextureLocation(DustBunnyEntity entity) {
-        String s = TextFormatting.stripFormatting(entity.getName().getString());
+        String s = ChatFormatting.stripFormatting(entity.getName().getString());
         if (s != null && "Big Chungus".equals(s)) {
             return BIG_CHUNGUS;
         }
--- src/main/java/teamdraco/dusted/client/render/EvilDustBunnyRenderer.java
+++ src/main/java/teamdraco/dusted/client/render/EvilDustBunnyRenderer.java
@@ -1,12 +1,11 @@
 package teamdraco.dusted.client.render;
 
-import com.mojang.blaze3d.matrix.MatrixStack;
-import net.minecraft.client.renderer.entity.EntityRendererManager;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.Mth;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import teamdraco.dusted.BellyButton;
@@ -16,14 +15,15 @@ import teamdraco.dusted.common.entities.EvilDustBunnyEntity;
 @OnlyIn(Dist.CLIENT)
 public class EvilDustBunnyRenderer extends MobRenderer<EvilDustBunnyEntity, DustBunnyModel<EvilDustBunnyEntity>> {
     private static final ResourceLocation TEXTURE = new ResourceLocation(BellyButton.MOD_ID, "textures/entity/evil_dust_bunny.png");
+    public static final ModelLayerLocation DUST_BUNNY = new ModelLayerLocation(new ResourceLocation(BellyButton.MOD_ID, "dust_bunny"), "main");
 
-    public EvilDustBunnyRenderer(EntityRendererManager renderManagerIn) {
-        super(renderManagerIn, new DustBunnyModel<>(), 0.35F);
+    public EvilDustBunnyRenderer(EntityRendererProvider.Context manager) {
+        super(manager, new DustBunnyModel<>(manager.bakeLayer(DUST_BUNNY)), 0.35F);
     }
 
-    protected void scale(EvilDustBunnyEntity entity, MatrixStack matrixStackIn, float partialTickTime) {
+    protected void scale(EvilDustBunnyEntity entity, PoseStack matrixStackIn, float partialTickTime) {
         float f1 = (float)entity.getSize();
-        float f2 = MathHelper.lerp(partialTickTime, entity.prevSquishFactor, entity.squishFactor) / (f1 * 0.5F + 1.0F);
+        float f2 = Mth.lerp(partialTickTime, entity.prevSquishFactor, entity.squishFactor) / (f1 * 0.5F + 1.0F);
         float f3 = 1.0F / (f2 + 1.0F);
         matrixStackIn.scale(f3 * f1, 1.0F / f3 * f1, f3 * f1);
     }
